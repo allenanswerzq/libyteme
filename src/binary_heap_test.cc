a7cb74d2ec6758d02c81834b17059e16cb85b377
landcold7 <landcold7@gmail.com>
2019-11-16 20:36:41 +0800

    Add deatch test for binary heap

--- src/binary_heap_test.cc
+++ src/binary_heap_test.cc
@@ -14,7 +14,7 @@ TEST(BinaryHeap, Insert) {
   EXPECT_EQ(binary_heap.hs, 0);
 }
 
-TEST(BinaryHeap, InitalizeVector) {
+TEST(BinaryHeap, InitializeVector) {
   vector<int> v = {4, 3, 1, 2, 5};
   BinaryHeap<100> binary_heap(v);
   EXPECT_EQ(binary_heap.hs, 5);
@@ -23,3 +23,21 @@ TEST(BinaryHeap, InitalizeVector) {
   }
   EXPECT_EQ(binary_heap.hs, 0);
 }
+
+TEST(DeathTest, ExceedHeap) {
+  vector<int> v = {4, 3, 1, 2, 5};
+  BinaryHeap<100> binary_heap(v);
+  EXPECT_EQ(binary_heap.hs, 5);
+  for (int i = 0; i < 6; i++) {
+    if (i >= 5) {
+      EXPECT_DEATH({
+        binary_heap.top();
+      }, "Assertion failed");
+    }
+    else {
+      EXPECT_EQ(binary_heap.top(), i + 1);
+    }
+  }
+  EXPECT_EQ(binary_heap.hs, 0);
+}
+
