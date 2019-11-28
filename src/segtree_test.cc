45b138d66d75f1f2ce2a463ddcdf2ff92349564c
landcold7 <landcold7@gmail.com>
2019-11-28 23:56:54 +0800

    Add logging, benchmark and

--- src/segtree_test.cc
+++ src/segtree_test.cc
@@ -1,17 +1,37 @@
 #include "segtree.h"
+
+#include "random.h"
+#include "benchmark.h"
 #include "gtest/gtest.h"
 
 namespace {
 
 TEST(Segtree, IntervalSum) {
   Segtree sg(10);
-  // 0 1 2 3
-  // 1 1 0 0
-  // 1 3 2 0
   sg.modify(0, 2, 1);
   EXPECT_EQ(sg.get(0, 2).sum, 2);
   sg.modify(1, 3, 2);
   EXPECT_EQ(sg.get(0, 3).sum, 6);
 }
+
+static void BM_SegtreeSum(int iter, int arg) {
+  int n = arg;
+  Segtree sg(n);
+  for (int i = 0; i < iter; i++) {
+    int lo = New64() % n;
+    int hi = New64() % n;
+    int val = New64() % n;
+    if (lo > hi) {
+      swap(lo, hi);
+   }
+    sg.modify(lo, hi, val);
+  }
+}
+
+BENCHMARK(BM_SegtreeSum)->Range(1000, 100000);
+
+// static BM_SegtreeSumVector(int iter) {
+//   Segtree
+// }
 
 }  // namespace
