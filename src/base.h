3cebe4598e845d40d7a7e6345872f2974b708b7f
landcold7 <landcold7@gmail.com>
2019-11-30 21:03:44 +0800

    Add a handy base.h include file

---
+++ src/base.h
@@ -0,0 +1,14 @@
+#ifndef BYTEME_BASE_H
+#define BYTEME_BASE_H
+
+#include <bits/stdc++.h>
+
+#include "types.h"
+#include "random.h"
+#include "logging.h"
+#include "benchmark.h"
+#include "gtest/gtest.h"
+
+using namespace std;
+
+#endif  // BYTEME_BASE_H
