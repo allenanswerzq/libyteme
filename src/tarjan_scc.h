cc4bcda8aa4e0389e3f4d7b8d3d07c766840c674
landcold7 <landcold7@gmail.com>
2019-12-24 23:09:58 +0800

    Fix tarjan scc algorithm

--- src/tarjan_scc.h
+++ src/tarjan_scc.h
@@ -1,60 +1,74 @@
 #include "base.h"
 
-template <class T>
-struct NodeSCC {
+struct NodeScc {
   int id;
   int index = -1;
   int lowlink = 1 << 30;
-  NodeSCC(int id_) : id(id_) {}
+  bool onstack = false;
+  NodeScc(int id_) : id(id_) {}
 };
 
 template<class T>
-using GraphSCC = GraphNode<NodeSCC, T>;
+using GraphScc = GraphNode<NodeScc, T>;
 
 // Compute strongly connected components.
 template<class T>
 struct TarjanScc {
   int index = 0;
-  GraphSCC<T>* g = nullptr;
+  GraphScc<T>* g = nullptr; // not owned
+
   // For each node, will assign a scc group id to it.
   vector<int> scc;
   int scc_cnt = 0;
-  stack<NodeScc> stk;
+  stack<NodeScc*> stk;
 
-  TarjanScc(const GraphSCC<T>* g_) : g(g_) {
-    scc.resize(g.n, -1);
+  explicit TarjanScc(GraphScc<T>* g_) : g(g_) {
+    scc.resize(g->n, -1);
   }
 
   vector<int> compute_scc() {
-    for (auto node : g.nodes) {
-      if (scc[node.id] == -1) {
+    index = 0;
+    for (auto node : g->nodes) {
+      assert(node);
+      assert(0 <= node->id && node->id < g->n);
+      if (scc[node->id] == -1) {
         strongconnect(node);
       }
     }
     return scc;
   }
 
-  void strongconnect(NodeScc v) {
-    v.index = index;
-    v.lowlink = index;
+  void strongconnect(NodeScc* v) {
+    assert(v);
+    v->index = index;
+    v->lowlink = index;
     index += 1;
     stk.push(v);
+    v->onstack = true;
 
-    for (auto w : g->edges()) {
-      if (w.index = -1) {
-        strongconnect(w);
-        v.lowlink = min(v.lowlink, w.lowlink);
+    for (auto w : g->get_edges(v->id)) {
+      if (w->to->index == -1) {
+        strongconnect(w->to);
+        v->lowlink = min(v->lowlink, w->to->lowlink);
       }
-      else if (scc[i] != -1) {
-        v.lowlink = min(v.lowlink, w.index);
+      else if (w->to->onstack) {
+        v->lowlink = min(v->lowlink, w->to->index);
+      }
+      else {
+        // Do nothing.
       }
     }
+
     // If v is a root node, pop the stack and generate a SCC.
-    if (v.lowlink == v.index) {
+    if (v->lowlink == v->index) {
+      NodeScc* w;
       do {
-        scc[stk.pop().id] = scc_cnt;
-      } while (w.id != v.id);
-      ++scc_cnt;
+        w = stk.top();
+        stk.pop();
+        scc[w->id] = scc_cnt;
+        w->onstack = false;
+      } while (w->id != v->id);
+      scc_cnt++;
     }
   }
 };
