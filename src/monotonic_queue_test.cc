47a278f1adc3111c018c9b505d8594e45b896831
landcold7 <landcold7@gmail.com>
2019-12-25 23:20:12 +0800

    Add monotonic queue

---
+++ src/monotonic_queue_test.cc
@@ -0,0 +1,23 @@
+#include "base.h"
+#include "monotonic_queue.h"
+
+namespace {
+
+TEST(Max, TestQueue) {
+  vector<int> v = {1, 10, 2, 8, 7, 3, 12};
+  MonotonicQueue mq;
+  vector<int> ans = mq.get_max(v, 3);
+  vector<int> right = {1, 10, 10, 10, 8, 8, 12};
+  EXPECT_EQ(ans, right);
+}
+
+
+TEST(LargestRectangle, TestStack) {
+  vector<int> height = {2, 1, 5, 6, 2, 3};
+  vector<int> ans = largest(height);
+  EXPECT_EQ(ans, 10);
+}
+
+}  // namespace
+
+}  // namespace
